a99d44a24b4702be8679d8fa475d37774ba22c53
Akshunna Prakash <fastmixes291@gmail.com>
2021-11-24 23:28:28 +0530

    Update read_name.c

--- 3_Implementation/src/read_name.c
+++ 3_Implementation/src/read_name.c
@@ -9,7 +9,7 @@
  * 
  */
 
-#include "/home/runner/work/M1_Application_Bus-Ticket-Reservation/M1_Application_Bus-Ticket-Reservation/3_Implementation/inc/read_name.h"
+#include 'D:/a/M1_Application_Bus-Ticket-Reservation/M1_Application_Bus-Ticket-Reservation/3_Implementation/inc/read_name.h'
 
 /*for putting the numeric value in the array*/
 void read_name(int trno)
